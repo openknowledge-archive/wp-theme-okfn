1fd77363fafff8a935efdd4a1a13c9e0e5b4c70d
stefie doll <stefie.doll@googlemail.com>
2010-07-19 19:24:09 +0200

    changes from 13/07/2010

--- footer.php
+++ footer.php
@@ -25,14 +25,9 @@
 		</div><!-- #colophon -->
 	</div><!-- #footer -->
 </div><!-- #wrapper -->
-
 <?php
-	/* Always have wp_footer() just before the closing </body>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to reference JavaScript files.
-	 */
-
 	wp_footer();
 ?>
+
 </body>
 </html>
--- homepage.php
+++ homepage.php
@@ -9,48 +9,16 @@ Template Name: Homepage
 			<div id="content" role="main">
 				<h1 class="entry-title">From sonnets to statistics, genes to geodata</h1>
 					<div class="entry-content">
-						<img src="<?php bloginfo('url'); ?>/wp-content/uploads/front_animation_bg.png">
+						<img src="<?php bloginfo('url'); ?>/wp-content/themes/okfn/images/front_animation_bg.png">
 					</div>
 					<h1 class="entry-title">What we are working on now?</h1>
 					<div class="entry-content">
-						<?php wp_list_pages( 'title_li=&child_of=4' ); ?>
+						<img src="<?php bloginfo('url'); ?>/wp-content/themes/okfn/images/dummy_project_listing.png" alt="Dummy Ptoject Listing" />
 						
-						<?php 
-						$projects =& get_children( 'post_type=page&post_parent=4', ARRAY_A ); 
-						if ( empty($projects) ) {
-							// no subpages
-						} else {
-							foreach ( $projects as $page_id) {
-								print ('<div>');
-								echo $page_id;
-								print ('</div>');
-							}
-						}
-						
-						?>
-						 
-						<!-- 
-						PLUGIN:
-						1. get all children of projects
-						1.1 get page id's
-						1.2 save in array
-						2. LOOP for array
-						2.1 get all titles
-						2.2 get all post_thumbnails
-						2.3 get custom_field short description
-						2.4 print them all in divs (odd/even)
-						/LOOP
-						
-						<?php query_posts('page_id=17'); ?>
-						    <?php while (have_posts()) : the_post(); ?>
-						    <?php the_post_thumbnail(array('40', '40')); ?>
-						    <?php endwhile; ?>
-						-->
-						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+					<!-- 	<?php wp_list_pages( 'title_li=&child_of=4' ); ?>
+
+						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>  -->
 					</div>    <!-- .entry-content -->
-
-
-
 			</div><!-- #content -->
 		</div><!-- #container -->
 
