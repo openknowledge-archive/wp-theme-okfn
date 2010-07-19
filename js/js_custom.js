2a96abcd6e62d72e691fed44fb0c31fd206d30d3
stefie doll <stefie.doll@googlemail.com>
2010-07-19 19:22:01 +0200

    changes from 13/07/2010

---
+++ js/js_custom.js
@@ -0,0 +1,28 @@
+$j=jQuery.noConflict();
+
+/** rounded corners **/
+$j(document).ready(function() {
+		$j('#primary li.widget-container').corner({
+		  tl: { radius: 10 },
+		  tr: { radius: 10 },
+		  bl: { radius: 10 },
+		  br: { radius: 10 }});
+		$j('.aktt_tweets li').corner({
+		  tl: { radius: 10 },
+		  tr: { radius: 10 },
+		  bl: { radius: 10 },
+		  br: { radius: 10 }});
+		$j('wpng-cal-widget-events').corner({
+		  tl: { radius: 10 },
+		  tr: { radius: 10 },
+		  bl: { radius: 10 },
+		  br: { radius: 10 }});
+		$j('#slideshow').corner({
+		  tl: { radius: 20 },
+		  tr: { radius: 20 },
+		  bl: { radius: 20 },
+		  br: { radius: 20 }});
+		$j('#loopedSlider a.previous').corner();
+		$j('#loopedSlider a.next').corner();
+	
+});
