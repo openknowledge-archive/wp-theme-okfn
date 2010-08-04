04fe71e885a821b2a817ae36b31556dcf576596a
stefie doll <stefie.doll@googlemail.com>
2010-08-04 00:36:20 +0000

    [homepage.php]added text to bubbles

--- homepage.php
+++ homepage.php
@@ -8,11 +8,19 @@ Template Name: Homepage
 		<div id="container">
 			<div id="content" role="main">
 		      <h1 class="entry-title">From sonnets to statistics, genes to geodata</h1>
-					<div class="entry-content">
-						<img src="<?php bloginfo('url'); ?>/wp-content/themes/okfn/images/front_animation_bg.png" alt="Frontpage Image" />
+					<div class="entry-content" id="frontimage">
+						
+						<img src="<?php bloginfo('url'); ?>/wp-content/themes/okfn/images/front_animation_bg.png" alt="Frontpage Image Bubbles" />
+						<div id="bubble_projects" class="bubble_links"><a href="/projects">Projects</a></div>
+						<div id="bubble_community" class="bubble_links"><a href="http://wiki.okfn.org" target="_blank">Community</a></div>
+						<div id="bubble_about" class="bubble_links"><a href="/about">About</a></div>
+						<div id="bubble_events" class="bubble_links"><a href="/calendar">Events</a></div>
+						<div id="bubble_news" class="bubble_links"><a href="http://blog.okfn.org" target="_blank">News</a></div>
 					</div>
-					<h1 class="entry-title">What we are working on now?</h1>
+
+					<h1 class="entry-title"><?php the_title(); ?></h1>
 					<div class="entry-content">
+						
 						<img src="<?php bloginfo('url'); ?>/wp-content/themes/okfn/images/dummy_project_listing.png" alt="Dummy Project Listing" />
 
 					</div>    <!-- .entry-content -->
