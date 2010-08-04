7615e058f1acf3d86c7cf001ef987fdf3da4b3a8
stefie doll <stefie.doll@googlemail.com>
2010-08-04 12:48:48 +0000

    [js_custom.js/homepage.php/ style.css] jquery animation for frontpage

--- homepage.php
+++ homepage.php
@@ -17,13 +17,62 @@ Template Name: Homepage
 						<div id="bubble_events" class="bubble_links"><a href="/calendar">Events</a></div>
 						<div id="bubble_news" class="bubble_links"><a href="http://blog.okfn.org" target="_blank">News</a></div>
 					</div>
-
-					<h1 class="entry-title"><?php the_title(); ?></h1>
-					<div class="entry-content">
-						
-						<img src="<?php bloginfo('url'); ?>/wp-content/themes/okfn/images/dummy_project_listing.png" alt="Dummy Project Listing" />
-
+					
+					
+					<div id="projects_front" class="frontpage_listing">
+						<h1 class="entry-title">What are we working on now?</h1>
+						<div class="entry-content">
+							<div class="odd"><h4><a href="#" target="_blank">CKAN</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="even"><h4><a href="#" target="_blank">Where Does My Money go?</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="odd"><h4><a href="#" target="_blank">The Open Definition Site</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="even"><h4><a href="#" target="_blank">Hyperlinked Title</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="odd"><h4><a href="#" target="_blank">Hyperlinked Title</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="even"><h4><a href="#" target="_blank">Hyperlinked Title</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="odd"><h4><a href="#" target="_blank">Hyperlinked Title</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
 					</div>    <!-- .entry-content -->
+				</div> <!--#projects_front -->
+				
+				<div id="about_front" class="frontpage_listing">
+						<h1 class="entry-title">What is the Open Knowledge Foudation?</h1>
+						<div class="entry-content">
+							<div class="odd"><h4><a href="#" target="_blank">People</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="even"><h4><a href="#" target="_blank">Idea</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="odd"><h4><a href="#" target="_blank">Hyperlinked Title</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="even"><h4><a href="#" target="_blank">Hyperlinked Title</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+					</div>    <!-- .entry-content -->
+				</div> <!--#about_front -->
+				
+				<div id="community_front" class="frontpage_listing">
+						<h1 class="entry-title">How to get involved?</h1>
+						<div class="entry-content">
+							<div class="odd"><h4><a href="#" target="_blank">OKFN Wiki</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="even"><h4><a href="#" target="_blank">Weekly IRC Meetings</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="odd"><h4><a href="#" target="_blank">Mailinglists</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="even"><h4><a href="#" target="_blank">Working Group Meetings</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="odd"><h4><a href="#" target="_blank">Hyperlinked Title</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+					</div>    <!-- .entry-content -->
+				</div> <!--#community_front -->
+				
+					<div id="news_front" class="frontpage_listing">
+						<h1 class="entry-title">What's new?</h1>
+						<div class="entry-content">
+							<div class="odd"><h4><a href="#" target="_blank">Blog</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="even"><h4><a href="#" target="_blank">News</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="odd"><h4><a href="#" target="_blank">Hyperlinked Title</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+				
+					</div>    <!-- .entry-content -->
+				</div> <!--#news_front -->
+
+					<div id="events_front" class="frontpage_listing">
+						<h1 class="entry-title">What is happening?</h1>
+						<div class="entry-content">
+							<div class="odd"><h4><a href="#" target="_blank">IRC Meetings</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="even"><h4><a href="#" target="_blank">Working Groups Meetings</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="odd"><h4><a href="#" target="_blank">Project Meetings</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+							<div class="even"><h4><a href="#" target="_blank">Related Events</a></h4><p>Summary linking to more. This is dummy text. Anything can go here! Short description of project, WG or anything else...</p></div>
+					</div>    <!-- .entry-content -->
+				</div> <!--#events_front -->
+				<div style="clear:both;"></div>
 			</div><!-- #content -->
 		</div><!-- #container -->
 
--- js/js_custom.js
+++ js/js_custom.js
@@ -23,5 +23,61 @@ $j(document).ready(function() {
 		  bl: { radius: 20 },
 		  br: { radius: 20 }});
 		$j('#loopedSlider a.previous').corner();
-		$j('#loopedSlider a.next').corner();	
+		$j('#loopedSlider a.next').corner();
+		
+/** homepage animation **/	
+	$j('#projects_front').css('display', 'block');
+	$j('#bubble_projects a').css('font-size', '1.2em');
+	
+	function hideall(){
+		$j('.frontpage_listing').css('display','none');
+		$j('div.bubble_links a').css('font-size', '1em');
+	}
+	
+	$j('#bubble_about a').hover(function(){
+		hideall();
+		$j(this).css('font-size', '1.2em');
+		$j(this).css('font-weight', 'bold');
+		$j('#about_front').css('display', 'block');
+	}, function(){
+		$j(this).css('font-weight', 'normal');
+	});
+	
+	$j('#bubble_community a').hover(function(){
+		hideall();
+		$j(this).css('font-size', '1.2em');
+		$j(this).css('font-weight', 'bold');
+		$j('#community_front').css('display', 'block');
+	}, function(){
+			$j(this).css('font-weight', 'normal');
+		});
+	
+	$j('#bubble_projects a').hover(function(){
+		hideall();
+		$j(this).css('font-size', '1.2em');
+		$j(this).css('font-weight', 'bold');
+		$j('#projects_front').css('display', 'block');
+	}, function(){
+			$j(this).css('font-weight', 'normal');
+		});
+
+	
+	$j('#bubble_news a').hover(function(){
+		hideall();
+		$j(this).css('font-size', '1.2em');
+		$j(this).css('font-weight', 'bold');
+		$j('#news_front').css('display', 'block');
+	}, function(){
+			$j(this).css('font-weight', 'normal');
+		});
+	
+	$j('#bubble_events a').hover(function(){
+		hideall();
+		$j(this).css('font-size', '1.2em');
+		$j(this).css('font-weight', 'bold');
+		$j('#events_front').css('display', 'block');
+	}, function(){
+			$j(this).css('font-weight', 'normal');
+		});
+	
 });
