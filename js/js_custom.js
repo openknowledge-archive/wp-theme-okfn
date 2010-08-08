1707b1068cba8b48b7c830f8ec3bddeb494d6092
rgrp <rufus.pollock@okfn.org>
2010-08-08 19:13:12 +0100

    [js/js_custom.js][s]: tidy up formatting of js_custom.js.

--- js/js_custom.js
+++ js/js_custom.js
@@ -1,88 +1,84 @@
-$j=jQuery.noConflict();
-
 /** rounded corners **/
-$j(document).ready(function() {
-		$j('#header').corner({
-		  tl: { radius: 0 },
-		  tr: { radius: 0 },
-		  bl: { radius: 10 },
-		  br: { radius: 10 }});
-		$j('#primary li.widget-container').corner({
-		  tl: { radius: 10 },
-		  tr: { radius: 10 },
-		  bl: { radius: 10 },
-		  br: { radius: 10 }});
-		$j('.aktt_tweets li').corner({
-		  tl: { radius: 10 },
-		  tr: { radius: 10 },
-		  bl: { radius: 10 },
-		  br: { radius: 10 }});
-		$j('#wpng-cal-widget-events').corner({
-		  tl: { radius: 10 },
-		  tr: { radius: 10 },
-		  bl: { radius: 10 },
-		  br: { radius: 10 }});
-		$j('#slideshow').corner({
-		  tl: { radius: 20 },
-		  tr: { radius: 20 },
-		  bl: { radius: 20 },
-		  br: { radius: 20 }});
-		$j('#loopedSlider a.previous').corner();
-		$j('#loopedSlider a.next').corner();
+jQuery(document).ready(function() {
+	jQuery('#header').corner({
+	  tl: { radius: 0 },
+	  tr: { radius: 0 },
+	  bl: { radius: 10 },
+	  br: { radius: 10 }});
+	jQuery('#primary li.widget-container').corner({
+	  tl: { radius: 10 },
+	  tr: { radius: 10 },
+	  bl: { radius: 10 },
+	  br: { radius: 10 }});
+	jQuery('.aktt_tweets li').corner({
+	  tl: { radius: 10 },
+	  tr: { radius: 10 },
+	  bl: { radius: 10 },
+	  br: { radius: 10 }});
+	jQuery('#wpng-cal-widget-events').corner({
+	  tl: { radius: 10 },
+	  tr: { radius: 10 },
+	  bl: { radius: 10 },
+	  br: { radius: 10 }});
+	jQuery('#slideshow').corner({
+	  tl: { radius: 20 },
+	  tr: { radius: 20 },
+	  bl: { radius: 20 },
+	  br: { radius: 20 }});
+	jQuery('#loopedSlider a.previous').corner();
+	jQuery('#loopedSlider a.next').corner();
 		
-/** homepage animation **/	
-	$j('#projects_front').css('display', 'block');
-	$j('#bubble_projects a').css('font-size', '1.2em');
+	/** homepage animation **/	
+	jQuery('#projects_front').css('display', 'block');
+	jQuery('#bubble_projects a').css('font-size', '1.2em');
 	
-	function hideall(){
-		$j('.frontpage_listing').css('display','none');
-		$j('div.bubble_links a').css('font-size', '1em');
+	function hideall() {
+		jQuery('.frontpage_listing').css('display','none');
+		jQuery('div.bubble_links a').css('font-size', '1em');
 	}
 	
-	$j('#bubble_about a').hover(function(){
+	jQuery('#bubble_about a').hover(function() {
 		hideall();
-		$j(this).css('font-size', '1.2em');
-		$j(this).css('font-weight', 'bold');
-		$j('#about_front').css('display', 'block');
-	}, function(){
-		$j(this).css('font-weight', 'normal');
+		jQuery(this).css('font-size', '1.2em');
+		jQuery(this).css('font-weight', 'bold');
+		jQuery('#about_front').css('display', 'block');
+		}, function() {
+			jQuery(this).css('font-weight', 'normal');
 	});
 	
-	$j('#bubble_community a').hover(function(){
+	jQuery('#bubble_community a').hover(function() {
 		hideall();
-		$j(this).css('font-size', '1.2em');
-		$j(this).css('font-weight', 'bold');
-		$j('#community_front').css('display', 'block');
-	}, function(){
-			$j(this).css('font-weight', 'normal');
-		});
+		jQuery(this).css('font-size', '1.2em');
+		jQuery(this).css('font-weight', 'bold');
+		jQuery('#community_front').css('display', 'block');
+		}, function() {
+			jQuery(this).css('font-weight', 'normal');
+	});
 	
-	$j('#bubble_projects a').hover(function(){
+	jQuery('#bubble_projects a').hover(function() {
 		hideall();
-		$j(this).css('font-size', '1.2em');
-		$j(this).css('font-weight', 'bold');
-		$j('#projects_front').css('display', 'block');
-	}, function(){
-			$j(this).css('font-weight', 'normal');
-		});
-
+		jQuery(this).css('font-size', '1.2em');
+		jQuery(this).css('font-weight', 'bold');
+		jQuery('#projects_front').css('display', 'block');
+		}, function() {
+			jQuery(this).css('font-weight', 'normal');
+	});
 	
-	$j('#bubble_news a').hover(function(){
+	jQuery('#bubble_news a').hover(function() {
 		hideall();
-		$j(this).css('font-size', '1.2em');
-		$j(this).css('font-weight', 'bold');
-		$j('#news_front').css('display', 'block');
-	}, function(){
-			$j(this).css('font-weight', 'normal');
-		});
+		jQuery(this).css('font-size', '1.2em');
+		jQuery(this).css('font-weight', 'bold');
+		jQuery('#news_front').css('display', 'block');
+		}, function() {
+			jQuery(this).css('font-weight', 'normal');
+	});
 	
-	$j('#bubble_events a').hover(function(){
+	jQuery('#bubble_events a').hover(function() {
 		hideall();
-		$j(this).css('font-size', '1.2em');
-		$j(this).css('font-weight', 'bold');
-		$j('#events_front').css('display', 'block');
-	}, function(){
-			$j(this).css('font-weight', 'normal');
-		});
-	
+		jQuery(this).css('font-size', '1.2em');
+		jQuery(this).css('font-weight', 'bold');
+		jQuery('#events_front').css('display', 'block');
+		}, function() {
+			jQuery(this).css('font-weight', 'normal');
+	});
 });
