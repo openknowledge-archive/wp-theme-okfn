d14d785533f3fe1fe36a65f8764f5514af87bfa9
stefie doll <stefie.doll@googlemail.com>
2010-08-10 12:29:41 +0000

    [functions.php] added favicon

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+function blog_favicon() {
+	echo '<link rel="Shortcut Icon" type="image/x-icon" href="http://okfn.org/images/favicon.ico" />';
+}
+add_action('wp_head', 'blog_favicon');
+
+?>
